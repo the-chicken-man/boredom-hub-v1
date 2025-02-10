b8e3c35d3b980f74cfb03092329b776732de030f
the-chicken-man <856030@dpsk12.net>
2025-02-10 12:13:47 -0700

    Add files via upload

---
+++ simple-3d-shooter/public/options.js
@@ -0,0 +1,16 @@
+window.initialGameOptions = {
+    
+    /**
+     * Main Options
+     */
+    
+    'debugMode': true, // Enable it to show debug messages on console
+
+    /**
+     * Colors (All in Hexadecimal)
+     */
+
+    // Object Colors
+    'backgroundColor': '#a29bfe',
+
+};
